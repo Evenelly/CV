9ad49ad6460d1dfd90eb0a4d942af1176294981e
Evelina Carlsson <evelina.carlsson1@learnet.se>
2024-02-14 12:47:02 +0100

    d

--- main.js
+++ main.js
@@ -22,3 +22,17 @@ function changeArrow(argId){
         z.setAttribute("d", "M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5");
     }
 }
+
+
+
+var ChangeImg = getElementById('växjö-img');
+var arrowHover = getElementsByClassName('kurser-arrow');
+
+arrowHover.addEventListener('mouseover', ()=>
+{
+    ChangeImg.style.maskImage = 'mask-image: linear-gradient(to top, transparent, rgb(0, 0, 0) 100%)';
+});
+arrowHover.addEventListener('mouseout', ()=>
+{
+    ChangeImg.style.maskImage = 'mask-image: linear-gradient(to top, transparent, rgb(0, 0, 0) 75%)'
+});
